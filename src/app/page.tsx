c36b016b339ac2636d6e5d7e9612cc4114c2511d
Monica personal <monacuaulma@gmail.com>
2024-11-22 23:07:27 -0400

    chore: change next config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+'use client';
 export default function Home() {
-    return <div>HOLA</div>;
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Trabajos U pty
+        </main>
+    );
 }
