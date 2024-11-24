82d24e91161b0acffae3e7c007851bef61341af9
Monica personal <monacuaulma@gmail.com>
2024-11-23 22:34:11 -0400

    chore: add folders structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import Navbar from './components/navbar/navbar';
 
 const poppins = Poppins({
     weight: ['400', '500', '600', '700'],
@@ -23,6 +24,7 @@ export default function RootLayout({
             <body
                 className={`${poppins.className} flex flex-col items-center w-full`}
             >
+                <Navbar />
                 {children}
             </body>
         </html>
