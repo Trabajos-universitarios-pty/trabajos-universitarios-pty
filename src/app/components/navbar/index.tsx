c869f6d84f2cbd7d52eaeb577e16281f1d8fbb06
Telnesito <carlosj_1703@hotmail.com>
2024-11-25 22:59:33 -0400

    chore: add navbar_items object

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,6 +1,4 @@
 const Navbar = () => {
-    const NAVBAR_ITEMS = [{}];
-
     return (
         <nav className="bg-rust-500 h-[64px] w-full">
             <ul className="flex gap-3">
---
+++ src/utils/navbar_items.tsx
@@ -0,0 +1,14 @@
+export const NAVBAR_ITEMS = [
+    {
+        id: 0,
+        name: 'Servicios'
+    },
+    {
+        id: 1,
+        name: 'Contactanos'
+    },
+    {
+        id: 2,
+        name: 'Nosotros'
+    }
+];
