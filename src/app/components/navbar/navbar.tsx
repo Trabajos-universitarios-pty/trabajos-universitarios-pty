65e829e18598ddab6468c888e4f5afe3a1e4c62a
Telnesito <carlosj_1703@hotmail.com>
2024-11-25 22:48:24 -0400

    chore: navbar styles

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,7 +1,9 @@
 const Navbar = () => {
+    const NAVBAR_ITEMS = [{}];
+
     return (
-        <nav>
-            <ul>
+        <nav className="bg-rust-500 h-[64px] w-full">
+            <ul className="flex gap-3">
                 <li>homa</li>
                 <li>home</li>
             </ul>
