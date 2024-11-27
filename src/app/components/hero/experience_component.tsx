48ee215877b96f5a80946f6690c1facb9f3e13a8
Monica personal <monacuaulma@gmail.com>
2024-11-26 22:38:56 -0400

    chore: add experience component

--- src/app/components/hero/experience_component.tsx
+++ src/app/components/hero/experience_component.tsx
@@ -1,9 +1,18 @@
 const ExperienceComponent = () => {
     return (
-        <div className="text-lg font-medium bg-gradient-to-r from-rust-600 to-carrot_orange-500 rounded-2xl w-fit p-4 text-white shadow-lg shadow-rust-300 hover:scale-105 transition-transform ">
-            <p className="text-w-shadow">+5 años de experiencia</p>
-            <p className="text-w-shadow">Calificaciones 100/100</p>
-            <p className="text-w-shadow">Antiplagio garantizado</p>
+        <div className="absolute z-10 -top-[86px] -left-[80px] text-base font-medium bg-gradient-to-r from-rust-600 to-carrot_orange-500 rounded-2xl w-fit p-4 text-white shadow-lg shadow-rust-500 hover:scale-105 transition-transform select-none ">
+            <div className="flex items-center">
+                <p className="text-w-shadow text-3xl">+5 &nbsp; </p>
+                <p className="text-w-shadow"> Años de experiencia</p>
+            </div>
+            <div className="flex items-center">
+                <p className="text-w-shadow">Calificaciones &nbsp;</p>
+                <p className="text-w-shadow text-3xl"> 100%</p>
+            </div>
+            <div className="flex items-center">
+                <p className="text-w-shadow text-3xl">🛇 &nbsp; </p>
+                <p className="text-w-shadow"> Antiplagio garantizado</p>
+            </div>
         </div>
     );
 };
