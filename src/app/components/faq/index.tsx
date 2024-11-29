504c1c0e7194476d5965e7ef1927a59264e5c8ce
Monica personal <monacuaulma@gmail.com>
2024-11-29 01:11:06 -0400

    chore: add id faq

--- src/app/components/faq/index.tsx
+++ src/app/components/faq/index.tsx
@@ -2,12 +2,12 @@ import FaqAccordion from './faq_accordion';
 
 const Faq: React.FC = () => {
     return (
-        <div className="w-full justify-center">
+        <section className="w-full justify-center" id="faq">
             <h1 className="text-4xl font-medium text-rust-100 mb-8 text-center txt-appear-animation">
                 Preguntas frecuentes
             </h1>
             <FaqAccordion />
-        </div>
+        </section>
     );
 };
 
