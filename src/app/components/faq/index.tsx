1628316271ddb4771d822ec010bf6e84a4b012d8
Monica personal <monacuaulma@gmail.com>
2024-11-29 12:26:02 -0400

    test

--- src/app/components/faq/index.tsx
+++ src/app/components/faq/index.tsx
@@ -4,7 +4,7 @@ const Faq: React.FC = () => {
     return (
         <section className="w-full justify-center" id="faq">
             <h1 className="text-4xl font-medium text-rust-100 mb-8 text-center txt-appear-animation">
-                Preguntas frecuentes
+                Preguntas frecuentes (prueba)
             </h1>
             <FaqAccordion />
         </section>
