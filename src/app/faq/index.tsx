928c5c0c7b90e67b7ab133413b52b3171eedf1d2
Telnesito <carlosj_1703@hotmail.com>
2024-11-29 22:49:14 -0400

    chore: style faq component

--- src/app/faq/index.tsx
+++ src/app/faq/index.tsx
@@ -2,7 +2,7 @@ import { FAQ } from '@/utils/faq_items';
 
 const Faq = () => {
     return (
-        <div className="mt-[200px] flex flex-col gap-6" id="faq">
+        <div className="mt-[200px] mb-[50px] flex flex-col gap-10" id="faq">
             <p className="text-3xl font-medium text-center">
                 Preguntas Frecuentes
             </p>
@@ -10,14 +10,28 @@ const Faq = () => {
                 {FAQ.map(({ description, title }, index) => (
                     <details
                         key={index}
-                        className={`p-4 bg-rust-600 text-white rounded shadow `}
+                        className="group p-4 bg-white hover:scale-[1.01] transition-all text-black rounded-lg shadow"
                     >
-                        <summary className="cursor-pointer text-lg font-semibold text-white">
-                            {title}
+                        <summary className="cursor-pointer text-rust-600 text-lg font-medium flex items-center justify-between">
+                            <span>{title}</span>
+                            <span className="transition-transform duration-400 transform group-open:rotate-180">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-5 h-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M19 9l-7 7-7-7"
+                                    />
+                                </svg>
+                            </span>
                         </summary>
-                        <div className={` mt-2 animate-fadeIn`}>
-                            {description}
-                        </div>
+                        <div className="mt-3">{description}</div>
                     </details>
                 ))}
             </div>
