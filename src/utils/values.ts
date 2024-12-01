dbb6ae05a77e118e8220cbff7c8aa1f7714b0fd1
Monica personal <monacuaulma@gmail.com>
2024-11-30 22:14:06 -0400

    chore: add values

---
+++ src/utils/values.ts
@@ -0,0 +1,22 @@
+export const VALUES = [
+    {
+        title: 'Servicio al cliente',
+        description:
+            'Estamos comprometidos en brindar un servicio de calidad y confianza, por ello, nos esforzamos en atender tus solicitudes de manera rápida y eficiente.'
+    },
+    {
+        title: 'Confidencialidad',
+        description:
+            'La privacidad de tus datos es nuestra prioridad, por ello, garantizamos la confidencialidad de tus trabajos y de tus datos personales.'
+    },
+    {
+        title: 'Calidad',
+        description:
+            'Nuestro equipo está comprometido en entregar los mejores resultados, el objetivo es la excelencia. Siempre buscamos alcanzar la calificación máxima.'
+    },
+    {
+        title: 'Garantía',
+        description:
+            'La satisfacción del cliente es nuestra prioridad. Es por ello que al comprometernos con una tarea, garantizamos la entrega en el plazo establecido, tarea original y libre de plagio, el seguimiento detallado de las instrucciones proporcionadas y la calificación mínima de aprobación.'
+    }
+];
