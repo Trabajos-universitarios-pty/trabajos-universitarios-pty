42e9c8a6cb704345f2da5e67b003ae2ab91835b0
Monica personal <monacuaulma@gmail.com>
2024-11-30 23:02:18 -0400

    chore: add styles to values card

--- src/utils/values.tsx
+++ src/utils/values.tsx
@@ -1,22 +1,29 @@
-export const VALUES = [
+import InfoIcon from '@/app/components/common/icons/info_icon';
+import { Value } from '@/shared/value';
+
+export const VALUES: Value[] = [
     {
+        icon: <InfoIcon />,
         title: 'Servicio al cliente',
         description:
             'Estamos comprometidos en brindar un servicio de calidad y confianza, por ello, nos esforzamos en atender tus solicitudes de manera rápida y eficiente.'
     },
     {
+        icon: <InfoIcon />,
         title: 'Confidencialidad',
         description:
             'La privacidad de tus datos es nuestra prioridad, por ello, garantizamos la confidencialidad de tus trabajos y de tus datos personales.'
     },
     {
+        icon: <InfoIcon />,
         title: 'Calidad',
         description:
             'Nuestro equipo está comprometido en entregar los mejores resultados, el objetivo es la excelencia. Siempre buscamos alcanzar la calificación máxima.'
     },
     {
+        icon: <InfoIcon />,
         title: 'Garantía',
         description:
-            'La satisfacción del cliente es nuestra prioridad. Es por ello que al comprometernos con una tarea, garantizamos la entrega en el plazo establecido, tarea original y libre de plagio, el seguimiento detallado de las instrucciones proporcionadas y la calificación mínima de aprobación.'
+            'La satisfacción del cliente es nuestra prioridad. Es por ello que al comprometernos con una tarea, garantizamos la entrega en el plazo establecido, tarea original y libre de plagio.'
     }
 ];
