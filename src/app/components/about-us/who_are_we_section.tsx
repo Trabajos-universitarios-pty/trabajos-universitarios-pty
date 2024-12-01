5bf1c5b531c0d33e3d4b6ff63536646428fdc8cf
Monica personal <monacuaulma@gmail.com>
2024-11-30 22:14:27 -0400

    chore: add who we are section

---
+++ src/app/components/about-us/who_are_we_section.tsx
@@ -0,0 +1,26 @@
+const WhoAreWeSection = () => {
+    return (
+        <div>
+            <h2>¿Quiénes somos?</h2>
+            <p>
+                Somos un equipo apasionado de profesionales y docentes con más
+                de 5 años de experiencia en el sector académico. Trabajos
+                universitarios PTY, se originó en Panamá, con la finalidad de
+                brindar un servicio de calidad y confianza a estudiantes
+                universitario.
+                <br />
+                Con el paso de los años y debido al rápido crecimiento de este
+                proyecto, nos hemos expandido por toda Latinoamérica, abarcando
+                diversos así países, como lo son: Panamá, Venezuela, Colombia,
+                México, entre otros.
+                <br />
+                Estamos capacitados con las herramientas necesarias y estándares
+                universales (normas APA, Vancouver, IEEE, entre otras) para
+                brindarte la ayuda que necesitas en tus trabajos universitarios,
+                tesis, proyectos de grado, ensayos, monografías, entre otros.
+            </p>
+        </div>
+    );
+};
+
+export default WhoAreWeSection;
