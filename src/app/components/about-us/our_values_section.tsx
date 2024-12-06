4bc0d785ee6aca44adc853e1f751de8e2e2d35b6
Monica personal <monacuaulma@gmail.com>
2024-12-06 09:17:33 -0400

    chore: change about us section

--- src/app/components/about-us/our_values_section.tsx
+++ src/app/components/about-us/our_values_section.tsx
@@ -3,15 +3,10 @@ import ValueCard from './value_card';
 
 const OurValues = () => {
     return (
-        <div className="flex flex-col lg:flex-row items-center gap-4 lg:gap-2 xl:gap-0">
-            <h2 className="text-4xl xl:text-center font-semibold lg:w-1/3 text-rust-500">
-                Nuestros valores
-            </h2>
-            <div className="grid sm:grid-cols-2 gap-2 md:gap-5 lg:w-2/3">
-                {VALUES.map((value, index) => (
-                    <ValueCard key={index} value={value} />
-                ))}
-            </div>
+        <div className="grid sm:grid-cols-2 gap-2 md:gap-5 lg:w-3/5">
+            {VALUES.map((value, index) => (
+                <ValueCard key={index} value={value} />
+            ))}
         </div>
     );
 };
