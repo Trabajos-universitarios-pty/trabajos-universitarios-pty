83b20a387ba22cbac48be8c261947ed391e0eab9
Monica personal <monacuaulma@gmail.com>
2024-12-06 13:44:55 -0400

    chore: add country flags

--- src/app/components/hero/hero_text_section.tsx
+++ src/app/components/hero/hero_text_section.tsx
@@ -4,7 +4,7 @@ const HeroTextSection: React.FC = () => {
     return (
         <div className="w-full lg:w-1/2 flex flex-col gap-6">
             <div className="gap-3">
-                <h3 className="text-xl txt-appear-animation">
+                <h3 className=" txt-appear-animation">
                     ¿Necesitas ayuda con tus tareas? ¡Te ayudamos!
                 </h3>
                 <h1 className="text-6xl font-medium txt-appear-animation">
