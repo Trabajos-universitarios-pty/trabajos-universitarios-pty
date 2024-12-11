b3cade573865affd8c950fd68f489ee2f9bb8f07
Monica personal <monacuaulma@gmail.com>
2024-12-11 10:36:18 -0400

    chore: change size of text

--- src/app/components/hero/hero_text_section.tsx
+++ src/app/components/hero/hero_text_section.tsx
@@ -4,11 +4,8 @@ const HeroTextSection: React.FC = () => {
     return (
         <div className="w-full lg:w-1/2 flex flex-col gap-6">
             <div className="gap-3">
-                <h3 className=" txt-appear-animation">
-                    ¿Necesitas ayuda con tus tareas? ¡Te ayudamos!
-                </h3>
-                <h1 className="text-6xl font-medium txt-appear-animation">
-                    Hacemos tu Tesis
+                <h1 className="text-5xl sm:text-6xl font-medium txt-appear-animation">
+                    Hacemos tu Tesis y Tareas
                 </h1>
                 <h2 className="text-3xl txt-appear-animation">
                     Encuentra a los mejores profesionales para hacer tus tareas
