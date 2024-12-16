0acee776fe5e81065091bde6dbec4ddd6dcd70c3
Monica personal <monacuaulma@gmail.com>
2024-12-15 22:58:19 -0400

    chore: add carousel

--- src/app/(pages)/(landing)/page.tsx
+++ src/app/(pages)/(landing)/page.tsx
@@ -1,17 +1,14 @@
 'use client';
 
 import AboutUs from '@/app/components/about-us';
-import Carousel from '@/app/components/carousel';
 import Hero from '@/app/components/hero';
 import Faq from '@/app/faq';
 import Services from '@/app/services';
-import { POSTS } from '@/utils/posts';
 
 export default function Landing() {
     return (
         <main className="flex  flex-col gap-8">
             <Hero />
-            <Carousel images={POSTS} />
             <AboutUs />
             <Services />
             <Faq />
--- src/app/components/carousel/index.tsx
+++ src/app/components/carousel/index.tsx
@@ -1,76 +1,30 @@
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
+import EmblaCarousel from './embla_carousel';
 
 interface CarouselProps {
     images: string[];
 }
 
 const Carousel = ({ images }: CarouselProps) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [itemsPerSlide, setItemsPerSlide] = useState(1);
-
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth > 640) {
-                setItemsPerSlide(2);
-            } else {
-                setItemsPerSlide(1);
-            }
-        };
-
-        handleResize(); // Llamamos a la función inicialmente para establecer el valor inicial
-        window.addEventListener('resize', handleResize);
-
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
-
-    const prevSlide = () => {
-        setCurrentIndex(
-            (prevIndex) => (prevIndex - 1 + images.length) % images.length
-        );
-    };
-
     return (
         <div className="relative w-full  overflow-hidden">
-            <button
-                className="absolute left-4 top-1/2 transform -translate-y-1/2 z-10 bg-gray-500 text-white px-4 py-2 rounded-full opacity-75 hover:opacity-100"
-                onClick={prevSlide}
-            >
-                Anterior
-            </button>
-            <div
-                className="flex transition-transform duration-500 ease-in-out"
-                style={{
-                    transform: `translateX(-${currentIndex * (100 / itemsPerSlide)}%)`
+            <EmblaCarousel
+                options={{
+                    loop: true
                 }}
-            >
-                {images.map((image, index) => (
-                    <div
+                slides={images.map((image, index) => (
+                    <Image
                         key={index}
-                        className={`w-full shrink-0 ${
-                            itemsPerSlide === 2 ? 'sm:w-1/2' : ''
-                        }`}
-                    >
-                        <Image
-                            src={image}
-                            alt={`Imagen ${index}`}
-                            width={500}
-                            height={300}
-                        />
-                    </div>
+                        src={image}
+                        alt="carousel image"
+                        width={500}
+                        height={300}
+                        objectFit="contain"
+                        unoptimized
+                        className="rounded-lg"
+                    />
                 ))}
-            </div>
-
-            <button
-                className="absolute right-4 top-1/2 transform -translate-y-1/2 z-10 bg-gray-500 text-white px-4 py-2 rounded-full opacity-75 hover:opacity-100"
-                onClick={nextSlide}
-            >
-                Siguiente
-            </button>
+            />
         </div>
     );
 };
