b862c3ac6609d3663dbbf4cd60c1784fc0ca85fe
Monica personal <monacuaulma@gmail.com>
2025-01-14 20:48:12 -0400

    chore: add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,26 @@ export default function RootLayout({
 }>) {
     return (
         <html className="scroll-smooth " lang="en">
+            <head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PR35C6FN');`
+                    }}
+                ></script>
+            </head>
             <body className={`${poppins.className} flex flex-col items-center`}>
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              <iframe  src="https://www.googletagmanager.com/ns.html?id=GTM-PR35C6FN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `
+                    }}
+                />
                 <Navbar />
                 <div className="w-full container mx-auto px-6 mt-4 lg:mt-0">
                     {children}
