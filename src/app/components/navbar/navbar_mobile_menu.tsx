09e432d5781f50c42cd0b198cbb4ebaafa383607
Monica personal <monacuaulma@gmail.com>
2025-01-18 22:40:18 -0400

    chore: remove cotiza button

--- src/app/components/navbar/navbar_mobile_menu.tsx
+++ src/app/components/navbar/navbar_mobile_menu.tsx
@@ -5,7 +5,6 @@ const NavbarMobileMenu: React.FC = () => {
     return (
         <div className="md:hidden bg-white flex flex-col w-full gap-2 min-h-[230px] justify-between p-5 transition-all rounded shadow-md">
             <NavbarLinks isMobile />
-            <CotizaButton />
         </div>
     );
 };
